e470813d5218c8e9ea66c4c6caa9e1ae5fdfcf00
miakojason <miakojason@gmail.com>
2024-03-05 14:44:25 +0800

    完成前台marquee

--- front/news.php
+++ front/news.php
@@ -1,6 +1,14 @@
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
     <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-    </marquee>
+    <?php
+        $ads = $Ad->all(['sh' => 1]);
+        foreach ($ads as $ad) {
+        ?>
+            <span><?= $ad['text']; ?>&nbsp;/&nbsp;</span>
+        <?php
+        }
+        ?>
+</marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
     <div style="text-align:center;">
