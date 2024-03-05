a7bc4132b3b99234a2f09d89133d5223d3b9f333
miakojason <miakojason@gmail.com>
2024-03-05 18:48:16 +0800

    api add+edit功能正常

--- api/edit.php
+++ api/edit.php
@@ -1,7 +1,7 @@
 <?php include_once "./db.php";
 $table = $_POST['table'];
 $DB = ${ucfirst($table)};
-foreach ($_POST['id'] as $key => $id) {
+foreach ($_POST['id'] as  $id) {
     if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
         $DB->del($id);
     } else {
