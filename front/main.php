d65c38283ea6d346fe1f94cbcdae26dec6d1d12a
miakojason <miakojason@gmail.com>
2024-03-05 15:17:18 +0800

    前台消息ok

--- front/main.php
+++ front/main.php
@@ -44,11 +44,24 @@
     </script>
     <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
+            <?php
+if($News->count(['sh'=>1])>5){
+    echo "<a href='?do=news' style='float:right'>more...</a>";
+}
+            ?>
         </span>
         <ul class="ssaa" style="list-style-type:decimal;">
-        <?php
-$news=$News->all(['sh'=>1])
-        ?>
+            <?php
+            $news = $News->all(['sh' => 1],"limit 5");
+            foreach ($news as $new) {
+            ?>
+            <li>
+                <div><?=mb_substr($new['text'],0,20);?>...</div>
+                <div class="all" style="display:none"><?=$new['text'];?></div>
+            </li>
+            <?php
+            }
+            ?>
         </ul>
         <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
         <script>
